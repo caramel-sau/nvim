178c514edc71f51aa4d9faff014a310803f1b940
caramel-sau <washing4563@gmail.com>
2025-01-02 16:37:14 +0900

    initial commit

---
+++ lua/keymaps.lua
@@ -0,0 +1,26 @@
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- split window
+map('n', '<Leader>s', ':sp<cr>', opts)
+map('n', '<Leader>v', ':vs<cr>', opts)
+
+-- move window
+map('n', '<Leader>j', '<C-w>j', opts)
+map('n', '<Leader>k', '<C-w>k', opts)
+map('n', '<Leader>l', '<C-w>l', opts)
+map('n', '<Leader>h', '<C-w>h', opts)
+
+-- Normal Mode
+map("n", "<ESC><ESC>", ":nohlsearch<CR>", opts)
+map("n", "gx", ":tabclose<CR>", opts)
+map("n", "gt", ":tabnew<CR>", opts)
+map("n", "gn", "gt", opts)
+map("n", "gp", "gT", opts)
+
+-- Insert Mode
+map('i', 'jj', '<ESC>', opts)
+
+-- show diagnostics
+map('n', '<Leader>d', ':lua vim.diagnostic.open_float()<CR>', opts)
+
