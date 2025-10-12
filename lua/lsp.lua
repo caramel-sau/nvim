1fe70098e04b05f3cd231abd4918f2c6d718652a
caramel-sau <washing4563@gmail.com>
2025-10-12 21:58:31 +0900

    feat: vueの設定追加

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -9,11 +9,13 @@ local lsp_servers = {
   "html",
   "jsonls",
   "ts_ls",
+  "vtsls",
   "marksman",
   "pylsp",
   "intelephense",
   "vue_ls",
   "emmet_language_server",
+  "eslint",
 }
 
 require("mason").setup({
@@ -29,6 +31,14 @@ require("mason").setup({
 require("mason-lspconfig").setup({
   ensure_installed = lsp_servers,
   capabilities = require("cmp_nvim_lsp").default_capabilities(),
+})
+
+-- vue_ls は事前に ts_ls と vtsls を設定しておく必要がある
+vim.lsp.config('ts_ls', {
+  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+})
+vim.lsp.config('vtsls', {
+  filetypes = { "javascript", "typescript", "vue" },
 })
 
 vim.lsp.enable(lsp_servers)
